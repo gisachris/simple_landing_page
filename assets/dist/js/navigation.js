6876d213616c436e65491f874339077ad56866a5
gisa chris <gisachris24@gmail.com>
2023-04-26 12:12:44 -0200

    add navigation controls for mobile version

---
+++ assets/dist/js/navigation.js
@@ -0,0 +1,31 @@
+// selecting the navigation
+const navigation = document.querySelector('.nav-together');
+//const navBar = document.querySelector('.nav-bar');
+const navItem = document.querySelectorAll('header .nav-together .nav-bar .nav-list li');
+const hamButton = document.querySelector('.ham-icon');
+const xButton = document.querySelector('.X-icon');
+
+
+hamButton.addEventListener('click',() => {
+  navigation.style.display = "flex";
+  xButton.style.display = "block";
+  hamButton.style.display = "none";
+});
+
+xButton.addEventListener('click',() => {
+navigation.style.display = "none";
+hamButton.style.display = "block";
+xButton.style.display = "none";
+});
+
+document.addEventListener('scroll',() =>{
+  navigation.style.display = "none";
+  hamButton.style.display = "";
+  xButton.style.display = "";
+});
+
+window.addEventListener('resize',() => {
+  navigation.style.display = "";
+  hamButton.style.display = "";
+  xButton.style.display = "";
+});
