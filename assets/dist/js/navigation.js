bcc05bd810ae80259de27683799ff1783bf1cf01
gisa chris <gisachris24@gmail.com>
2023-04-27 20:19:44 -0200

    fix linter errors

--- assets/dist/js/navigation.js
+++ assets/dist/js/navigation.js
@@ -1,31 +1,29 @@
 // selecting the navigation
 const navigation = document.querySelector('.nav-together');
-//const navBar = document.querySelector('.nav-bar');
-const navItem = document.querySelectorAll('header .nav-together .nav-bar .nav-list li');
+// const navBar = document.querySelector('.nav-bar');
 const hamButton = document.querySelector('.ham-icon');
 const xButton = document.querySelector('.X-icon');
 
-
-hamButton.addEventListener('click',() => {
-  navigation.style.display = "flex";
-  xButton.style.display = "block";
-  hamButton.style.display = "none";
+hamButton.addEventListener('click', () => {
+  navigation.style.display = 'flex';
+  xButton.style.display = 'block';
+  hamButton.style.display = 'none';
 });
 
-xButton.addEventListener('click',() => {
-navigation.style.display = "none";
-hamButton.style.display = "block";
-xButton.style.display = "none";
+xButton.addEventListener('click', () => {
+  navigation.style.display = 'none';
+  hamButton.style.display = 'block';
+  xButton.style.display = 'none';
 });
 
-document.addEventListener('scroll',() =>{
-  navigation.style.display = "";
-  hamButton.style.display = "";
-  xButton.style.display = "";
+document.addEventListener('scroll', () => {
+  navigation.style.display = '';
+  hamButton.style.display = '';
+  xButton.style.display = '';
 });
 
-window.addEventListener('resize',() => {
-navigation.style.display = "";
-  hamButton.style.display = "";
-  xButton.style.display = "";
+window.addEventListener('resize', () => {
+  navigation.style.display = '';
+  hamButton.style.display = '';
+  xButton.style.display = '';
 });
